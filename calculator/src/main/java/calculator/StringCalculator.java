8f813fef23470ea8fbe3cf6c89c3778e8e19afdf
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-06-13 02:25:37 +0900

    refactor: StringCalculator

--- calculator/src/main/java/calculator/StringCalculator.java
+++ calculator/src/main/java/calculator/StringCalculator.java
@@ -3,28 +3,50 @@ package calculator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringCalculator {
-    int sum = 0;
+import static java.lang.Integer.parseInt;
 
+public class StringCalculator {
     public int add(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
-        } else if (m.find()) {
-            String customDelimiter = m.group(1);
-            return add(m.group(2).split(customDelimiter));
-        } else {
-            return add(text.split(",|:"));
         }
+        return sum(toInts(getSplit(text)));
     }
 
-    public int add(String[] texts) {
-        for (String text : texts) {
-            if (Integer.parseInt(text) >= 0) {
-                sum += Integer.parseInt(text);
-            } else {
-                throw new RuntimeException();
-            }
+    private boolean isBlank(String text) {
+        return text.isEmpty();
+    }
+
+    private String[] getSplit(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            // 원본 문자열이 m.group(0)
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return text.split("[,:]");
+    }
+
+    private int[] toInts(String[] texts) {
+        int[] numbers = new int[texts.length];
+        for (int i = 0; i < texts.length; i++) {
+            numbers[i] = toPositive(texts[i]);
+        }
+        return numbers;
+    }
+
+    private int toPositive(String text) {
+        int number = parseInt(text);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
         }
         return sum;
     }
